eeab234ed60e4e6488344418c456adae0132e500
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-10 16:37:29 -0500

    SIZES created

--- constants/theme.js
+++ constants/theme.js
@@ -15,3 +15,12 @@ const FONT = {
     medium: "DMMedium",
     bold: "DMBold",
 };
+
+const SIZES = {
+    xSmall: 10,
+    small: 12,
+    medium: 16,
+    large: 20,
+    xLarge: 24,
+    xxLarge: 32,
+};
