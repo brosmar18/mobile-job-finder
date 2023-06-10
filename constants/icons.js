47af202925b1489467bbe7732c5975eb84392ffb
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-10 16:27:51 -0500

    filter setup

--- constants/icons.js
+++ constants/icons.js
@@ -1,10 +1,12 @@
 import heart from '../assets/icons.heart.png';
 import menu from '../assets/icons/menu.png';
 import search from '../assets/icons/search.png';
+import filter from '../assets/icons/filter.png';
 
 
 export default {
     heart,
     menu,
-    search
+    search,
+    filter
 };
