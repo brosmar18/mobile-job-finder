d34f7d16ab301d429714b47f496d72788394f24c
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-10 16:37:50 -0500

    SHADOWS created

--- constants/theme.js
+++ constants/theme.js
@@ -24,3 +24,26 @@ const SIZES = {
     xLarge: 24,
     xxLarge: 32,
 };
+
+const SHADOWS = {
+    small: {
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 2,
+    },
+    medium: {
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 5.84,
+        elevation: 5,
+    },
+};
