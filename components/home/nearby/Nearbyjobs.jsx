89d2f3dbafbaa82932c1b9106ef730f0bd39c6c2
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-10 17:00:06 -0500

    NearbyJobs basic functional component setup

---
+++ components/home/nearby/Nearbyjobs.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+
+import styles from './nearbyJobs.style';
+
+const NearbyJobs = () => {
+    return (
+        <View>
+            <Text>Nearby Jobs</Text>
+        </View>
+    )
+}
+
+export default NearbyJobs;
