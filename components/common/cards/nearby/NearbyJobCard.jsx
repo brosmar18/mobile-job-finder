0685691685e34f6aa9c01bffecc80ec9478e369d
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-10 16:50:33 -0500

    NearbyJobCard functional component setup

---
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import styles from './nearByJobCard.style';
+
+const NearbyJobCard = () => {
+    return (
+        <View>
+            <Text>Nearby Job Card</Text>
+        </View>
+    )
+}
+
+export default NearbyJobCard;
