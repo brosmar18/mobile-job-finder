5d16cd5a8c0450cc1fcd97ec1e5ea03897769a1e
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-10 16:53:48 -0500

    PopularJobCard basic funcitonal component setup

---
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import styles from './popularJobCard.style';
+
+const PopularJobCard = () => {
+    return (
+        <View>
+            <Text>Popular Job Card</Text>
+        </View>
+    )
+}
+
+export default PopularJobCard;
