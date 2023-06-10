7638d0cca02ffc5c8022957e6acf8db2c933c1cf
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-10 16:27:02 -0500

    search setup

--- constants/icons.js
+++ constants/icons.js
@@ -1,7 +1,10 @@
 import heart from '../assets/icons.heart.png';
 import menu from '../assets/icons/menu.png';
+import search from '../assets/icons/search.png';
+
 
 export default {
     heart,
-    menu
+    menu,
+    search
 };
