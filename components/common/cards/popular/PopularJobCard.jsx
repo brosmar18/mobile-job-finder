fb1086448de708c8989b8f190c8416a1d2c3f978
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-10 22:27:20 -0500

    job title section created

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -13,18 +13,29 @@ const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
             style={styles.container(selectedJob, item)}
             onPress={() => handleCardPress(item)}
         >
-            <TouchableOpacity
-                style={styles.logoContainer(selectedJob, item)}
-            >
-                <Image
-                    source={{
-                        uri: item.employer_logo}}
+            {item && item.employer_logo && item.job_title && (
+                <TouchableOpacity
+                    style={styles.logoContainer(selectedJob, item)}
+                >
+                    <Image
+                        source={{ uri: item.employer_logo }}
                         resizeMode='contain'
                         style={styles.logoImage}
-                />
-            </TouchableOpacity>
+                    />
+                </TouchableOpacity>
+            )}
+            <Text style={styles.companyName} numberOfLines={1}>
+                {item ? item.employer_name : ''}
+            </Text>
+
+            <View style={styles.infoContainer}>
+                <Text style={styles.jobName(selectedJob, item)} numberOfLines={1}>
+                    {item ? item.job_title : ''}
+                </Text>
+            </View>
         </TouchableOpacity>
     )
 }
+
 
 export default PopularJobCard;
