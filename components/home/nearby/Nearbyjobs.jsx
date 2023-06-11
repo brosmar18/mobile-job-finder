f9befb8e35bd778a6868e6c645395122e0c4450e
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-10 22:57:18 -0500

    header and show all added

--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useRouter } from 'expo-router';
 import {
     View,
     Text,
@@ -12,9 +13,20 @@ import NearbyJobCard from '../../common/cards/nearby/NearbyJobCard';
 import useFetch from '../../../hook/useFetch';
 
 const NearbyJobs = () => {
+    const router = useRouter();
+    const { data, isLoading, error } = useFetch("search", {
+        query: "React Native developer",
+        num_pages: "1"
+    });
+
     return (
-        <View>
-            <Text>Nearby Jobs</Text>
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.headerTitle}>Nearby Jobs</Text>
+                <TouchableOpacity>
+                    <Text style={styles.headerBtn}>Show all</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
