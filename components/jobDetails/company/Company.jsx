afaa766e448d9d2b657e23547f755a7db0b304a3
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-10 23:58:47 -0500

    jobdetails rendering

--- components/jobDetails/company/Company.jsx
+++ components/jobDetails/company/Company.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import { View, Text, Image } from 'react-native'
+import React from "react";
+import { View, Text, Image } from "react-native";
 
-import styles from './company.style'
-import { icons } from '../../../constants';
-import { checkImageURL } from '../../../utils';
+import styles from "./company.style";
+import { icons } from "../../../constants";
+import { checkImageURL } from "../../../utils";
 
 const Company = ({ companyLogo, jobTitle, companyName, location }) => {
     return (
@@ -12,18 +12,20 @@ const Company = ({ companyLogo, jobTitle, companyName, location }) => {
                 <Image
                     source={{
                         uri: checkImageURL(companyLogo)
-                        ? companyLogo
-                            : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg"
+                            ? companyLogo
+                            : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
                     }}
                     style={styles.logoImage}
+                    resizeMode="contain"
                 />
             </View>
-            <View style={styles.jobTitle}>
+
+            <View style={styles.jobTitleBox}>
                 <Text style={styles.jobTitle}>{jobTitle}</Text>
             </View>
 
             <View style={styles.companyInfoBox}>
-                <Text style={styles.companyName}>{companyName}</Text>
+                <Text style={styles.companyName}>{companyName} / </Text>
                 <View style={styles.locationBox}>
                     <Image
                         source={icons.location}
@@ -37,4 +39,4 @@ const Company = ({ companyLogo, jobTitle, companyName, location }) => {
     );
 };
 
-export default Company
+export default Company;
