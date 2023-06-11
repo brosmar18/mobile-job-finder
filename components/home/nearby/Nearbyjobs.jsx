d2b746c33740a1fc0d2c4e74de76d4b73669232c
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-10 22:53:44 -0500

    imports setup

--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    ActivityIndicator
+} from 'react-native';
 
 import styles from './nearbyJobs.style';
+import { COLORS } from '../../../constants';
+import NearbyJobCard from '../../common/cards/nearby/NearbyJobCard';
+import useFetch from '../../../hook/useFetch';
 
 const NearbyJobs = () => {
     return (
