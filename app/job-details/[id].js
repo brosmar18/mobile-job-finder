bbaf7540a52f784f949cdeb2ecca822f86e8780f
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-10 23:43:13 -0500

    Company props created

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -68,8 +68,15 @@ const JobDetails = () => {
                         <Text>No data available</Text>
                     ) : (
                         <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
-                            <Company />
-                            <JobTabs />
+                            <Company
+                                companyLogo={data[0].employer_logo}
+                                jobTitle={data[0].job_title}
+                                companyName={data[0].employer_name}
+                                location={data[0].job_country}
+                            />
+                            <JobTabs
+
+                            />
                         </View>
                     )}
                 </ScrollView>
