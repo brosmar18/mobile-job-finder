9be8e864511eda0f4506f3a283c595789bc291c9
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-10 23:26:00 -0500

    imports setup

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,4 +1,23 @@
-import { Text } from 'react-native';
+import {
+    Text,
+    View,
+    SafeAreaView,
+    ScrollView,
+    ActivityIndicator,
+    RefreshControl
+ } from 'react-native';
+ import { Stack, useRouter, useSearchParams } from 'expo-router';
+
+ import {
+    Company,
+    JobAbout,
+    JobFooter,
+    JobTabs,
+    ScreenHeaderBtn,
+    Specifics
+ } from '../../components';
+ import {COLORS, icons, SIZES} from '../../constants';
+ import useFetch from '../../hook/useFetch';
 
 const JobDetails = () => {
     return (
