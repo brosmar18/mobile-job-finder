810ffd487897f267e22e8ccb2d5bdf392a9e4490
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-06-10 19:25:26 -0500

    imports setup

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import { useState } from 'react';
+import {
+    Text,
+    View,
+    TextInput,
+    TouchableOpacity,
+    Image,
+    FlatList
+} from 'react-native';
+import { useRouter } from 'expo-router';
 
 import styles from './welcome.style';
+import { icons, SIZES } from '../../../constants';
 
 const Welcome = () => {
     return (
